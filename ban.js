57024dd0a8cd4eb7401c69bd9a8e8bb355ef0c3b
Mitannie <97797341+Mitannies@users.noreply.github.com>
2023-09-18 18:53:05 +0200

    Add files via upload

---
+++ ban.js
@@ -0,0 +1,69 @@
+const { EmbedBuilder } = require('discord.js');
+
+module.exports = {
+    name: 'ban',
+    description: 'Bannir un membre',
+    type: 1,
+    options: [
+        {
+            name: 'user',
+            description: 'Le membre a bannir',
+            type: 6,
+            required: true
+        },
+        {
+            name: 'reason',
+            description: 'La raison du bannissement',
+            type: 3,
+            required: false
+        }
+    ],
+    role_perms: ['914941428457689134'], //Vous devez metre le ID du role capable de bannir, comme le role STAFF
+    developers_only: false,
+    category: 'Moderation',
+    run: async (client, interaction, config) => {
+        const userInput = interaction.options.get('user').value;
+        const reasonInput = interaction.options.get('reason')?.value || 'Aucune raison fournie';
+
+        const user = interaction.guild.members.cache.get(userInput);
+
+        if (!user) return interaction.reply({
+            content: `\`❌\` L'utilisateur n'est pas/plus dans le serveur`,
+            ephemeral: true
+        });
+
+        if (!user.bannable) return interaction.reply({
+            content: `\`❌\` L'utilisateur n'est pas bannissable.`,
+            ephemeral: true
+        });
+
+        try {
+            await interaction.guild.members.ban(userInput, { reason: reasonInput });
+
+            user.send({
+                content: `Vous avez été banni de **${interaction.guild.name}**. ${reasonInput}`
+            }).catch(() => { });
+
+            interaction.reply({
+                content: `\`✅\` ${user} a été banni avec succès !`,
+                ephemeral: true
+            });
+
+            return interaction.channel.send({
+                embeds: [
+                    new EmbedBuilder()
+                        .setDescription(`${user} a été banni.`)
+                        .setColor('Red')
+                ]
+            });
+        } catch {
+            return interaction.reply({
+                content: `\`❌\` Quelque chose s'est mal passé !`,
+                ephemeral: true
+            });
+        };
+    }
+};
+
+// Code fait par Mitannie
+//Pour plus de code gratuit: https://github.com/Mitannies/France-Logistique
