62182c11b2a959d28183bfbe099b4cd9bc6b110e
Mitannie <97797341+Mitannies@users.noreply.github.com>
2023-09-19 13:05:32 +0200

    Update kick.js

--- slash/kick.js
+++ slash/kick.js
@@ -19,7 +19,7 @@ module.exports = {
         }
     ],
     role_perms: ['914941428457689134'], //Vous devez metre le ID du role capable de kick, comme le role STAFF
-    developers_only: false,
+    developers_only: false, //Autoriser le proprietaire du bot uniquement = true, sinon false (Par defaut)
     category: 'Moderation',
     run: async (client, interaction, config) => {
         const userInput = interaction.options.get('user').value;
@@ -64,3 +64,28 @@ module.exports = {
         };
     }
 };
+
+// Code fait par Mitannie
+// Pour plus de code gratuit: https://github.com/Mitannies/France-Logistique
+// Licence MIT:
+// MIT License
+
+// Copyright (c) 2023 France Logistique
+
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
